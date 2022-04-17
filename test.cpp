9acdbb0cbfc0d670878a556c1776af513fe9d7c5
George(PokidyshevYA) <PokidyshevYA@mpei.ru>
2022-04-17 19:10:06 +0300

    code: добавлены все тесты

--- test.cpp
+++ test.cpp
@@ -13,7 +13,43 @@ void test_positive(){
     // assert(max == 4);
 }
 
+void test_negative(){
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == 0);
+}
+
+void test_same(){
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 0);
+    assert(max == 1);
+}
+
+void test_one_num(){
+    double min = 0;
+    double max = 0;
+    find_minmax({-5}, min, max);
+    assert(min == -5);
+    assert(max == 0);
+}
+
+void test_glass_array(){
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
 int main() {
     test_positive();
+    test_negative();
+    test_same();
+    test_one_num();
+    test_glass_array();
     return 0;
 }
