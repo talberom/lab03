bf49bb664907bed2d5a465b7b54e0f1fc73dbb47
George(PokidyshevYA) <PokidyshevYA@mpei.ru>
2022-05-29 00:16:32 +0300

    code: добавлен параметр для вывода подсказок

--- main.cpp
+++ main.cpp
@@ -15,16 +15,21 @@ vector<double> input_numbers(istream& in, size_t count){
     return result;
 }
 
-Input read_input(istream& in){
+Input read_input(istream& in, bool prompt){
 
     Input data;
-
-    cerr << "Enter number count: ";
+    if (prompt){
+        cerr << "Enter number count: ";
+    }
     size_t number_count;
     in >> number_count;
-    cerr << "Enter numbers: ";
+    if (prompt){
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
-    cerr << "Enter bin count: ";
+    if (prompt){
+        cerr << "Enter bin count: ";
+    }
     size_t bin_count;
     in >> data.bin_count;
 
@@ -33,11 +38,8 @@ Input read_input(istream& in){
 
 int main()
 {
-    const auto data = read_input(cin);
-
+    const auto data = read_input(cin, true);
     const auto bins = make_histogram(data);
-
     show_histogram_svg(bins);
-
     return 0;
 }
